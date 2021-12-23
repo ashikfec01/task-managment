b8a6fc9ab8afe5fccdff8231872a1c55dfeb7a54
Kazi Ashik <dev.ashik333@gmail.com>
2021-12-24 05:00:16 +0600

    Pisma sqlite successfully implement

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 import { TasksModule } from './modules/tasks/tasks.module';
+import { PostService } from './post.service';
+import { PrismaService } from './prisma.service';
+import { UserService } from './user.service';
 
 @Module({
   imports: [TasksModule],
+  controllers: [AppController],
+  providers: [PrismaService, UserService, PostService]
 })
 export class AppModule { }
